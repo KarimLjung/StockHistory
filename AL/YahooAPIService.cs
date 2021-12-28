73657ab41ca6ec47385205d1ca02f44b8f25546e
Karim Ljung <karim.ljung@gmail.com>
2021-12-28 18:10:33 +0100

    Using new endpoint for ticker with current time

--- AL/YahooAPIService.cs
+++ AL/YahooAPIService.cs
@@ -8,7 +8,8 @@ namespace AL
     public class YahooAPIService : IYahooAPIService
     {
         private readonly IHttpClientFactory _httpClientFactory;
-        string yahooAPIhostname = "https://yfapi.net/v6/finance/quote";
+
+        string yahooAPIhostname = "https://yfapi.net/v7/finance/options/";
         public YahooAPIService(IHttpClientFactory httpClientFactory)
         {
             _httpClientFactory = httpClientFactory;
@@ -16,15 +17,16 @@ namespace AL
 
         public async Task<TickerInfo> GetStockInformationForTicker(string ticker)
         {
-            var parameters = "region=US&lang=en&symbols=AAPL%2CBTC-USD%2CEURUSD%3DX";
+            yahooAPIhostname += ticker;
             var client = _httpClientFactory.CreateClient();
             client.DefaultRequestHeaders.Add("accept", "application/json");
             client.DefaultRequestHeaders.Add("X-API-KEY", "oF7fRrwMxK5xcFmFlUKzl44aYDt7HpUd19AUstAj");
+
+            var unixTimestamp = (Int32)(DateTime.Now.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
+
             var query = new Dictionary<string, string>()
             {
-                {"region", "US" },
-                {"lang", "en" },
-                {"symbols", ticker }
+                {"date", unixTimestamp.ToString() }
             };
 
             var responseMessage = await client.GetAsync(
@@ -34,20 +36,26 @@ namespace AL
             {
                 var tickerString =
                     await responseMessage.Content.ReadAsStringAsync();
-                var yahooStockInfo = JsonSerializer.Deserialize<YahooStockInfo>(tickerString);
-                var tickerInfo = yahooStockInfo?.quoteResponse?.result?.
-                    Select(f => new TickerInfo { 
-                        Ticker = ticker,
-                        StockName = f.shortName, 
-                        Id = Guid.NewGuid().ToString(),
-                        LatestPrice = f.regularMarketPrice,
-                        LatestPriceDate = DateTime.UtcNow
-                    }).First();
+                var yahooStockInfoOptions = JsonSerializer.Deserialize<YahooStockInfoOptions>(tickerString);
+                TickerInfo? tickerInfo = CreateTickerInfo(ticker, yahooStockInfoOptions);
 
                 return tickerInfo;
             }
 
             return null;
+        }
+
+        private static TickerInfo? CreateTickerInfo(string ticker, YahooStockInfoOptions? yahooStockInfo)
+        {
+            return yahooStockInfo?.optionChain.result?.
+                                Select(f => new TickerInfo
+                                {
+                                    Ticker = ticker,
+                                    StockName = f.underlyingSymbol,
+                                    Id = Guid.NewGuid().ToString(),
+                                    LatestPrice = f.quote.regularMarketPrice,
+                                    LatestPriceDate = DateTime.UtcNow
+                                }).First();
         }
     }
 }
---
+++ AL/YahooStockInfoOptions.cs
@@ -0,0 +1,105 @@
+﻿
+public class YahooStockInfoOptions
+{
+    public Optionchain optionChain { get; set; }
+}
+
+public class Optionchain
+{
+    public Result[] result { get; set; }
+    public object error { get; set; }
+}
+
+public class Result
+{
+    public string underlyingSymbol { get; set; }
+    public decimal[] expirationDates { get; set; }
+    public object[] strikes { get; set; }
+    public bool hasMiniOptions { get; set; }
+    public Quote quote { get; set; }
+    public Option[] options { get; set; }
+}
+
+public class Quote
+{
+    public string language { get; set; }
+    public string region { get; set; }
+    public string quoteType { get; set; }
+    public string quoteSourceName { get; set; }
+    public bool triggerable { get; set; }
+    public string currency { get; set; }
+    public string exchange { get; set; }
+    public string longName { get; set; }
+    public string messageBoardId { get; set; }
+    public string exchangeTimezoneName { get; set; }
+    public string exchangeTimezoneShortName { get; set; }
+    public decimal gmtOffSetMilliseconds { get; set; }
+    public string market { get; set; }
+    public bool esgPopulated { get; set; }
+    public string marketState { get; set; }
+    public long firstTradeDateMilliseconds { get; set; }
+    public decimal priceHdecimal { get; set; }
+    public decimal trailingAnnualDividendYield { get; set; }
+    public decimal epsTrailingTwelveMonths { get; set; }
+    public decimal epsForward { get; set; }
+    public decimal epsCurrentYear { get; set; }
+    public decimal priceEpsCurrentYear { get; set; }
+    public decimal sourcedecimalerval { get; set; }
+    public decimal exchangeDataDelayedBy { get; set; }
+    public decimal pageViewGrowthWeekly { get; set; }
+    public string averageAnalystRating { get; set; }
+    public bool tradeable { get; set; }
+    public long sharesOutstanding { get; set; }
+    public decimal bookValue { get; set; }
+    public decimal fiftyDayAverage { get; set; }
+    public decimal fiftyDayAverageChange { get; set; }
+    public decimal fiftyDayAverageChangePercent { get; set; }
+    public decimal twoHundredDayAverage { get; set; }
+    public decimal twoHundredDayAverageChange { get; set; }
+    public decimal twoHundredDayAverageChangePercent { get; set; }
+    public long marketCap { get; set; }
+    public decimal forwardPE { get; set; }
+    public decimal priceToBook { get; set; }
+    public decimal regularMarketChange { get; set; }
+    public decimal regularMarketChangePercent { get; set; }
+    public decimal regularMarketTime { get; set; }
+    public decimal regularMarketPrice { get; set; }
+    public decimal regularMarketDayHigh { get; set; }
+    public string regularMarketDayRange { get; set; }
+    public decimal regularMarketDayLow { get; set; }
+    public decimal regularMarketVolume { get; set; }
+    public decimal regularMarketPreviousClose { get; set; }
+    public decimal bid { get; set; }
+    public decimal ask { get; set; }
+    public decimal bidSize { get; set; }
+    public decimal askSize { get; set; }
+    public string fullExchangeName { get; set; }
+    public string financialCurrency { get; set; }
+    public decimal regularMarketOpen { get; set; }
+    public decimal averageDailyVolume3Month { get; set; }
+    public decimal averageDailyVolume10Day { get; set; }
+    public decimal fiftyTwoWeekLowChange { get; set; }
+    public decimal fiftyTwoWeekLowChangePercent { get; set; }
+    public string fiftyTwoWeekRange { get; set; }
+    public decimal fiftyTwoWeekHighChange { get; set; }
+    public decimal fiftyTwoWeekHighChangePercent { get; set; }
+    public decimal fiftyTwoWeekLow { get; set; }
+    public decimal fiftyTwoWeekHigh { get; set; }
+    public decimal dividendDate { get; set; }
+    public decimal earningsTimestamp { get; set; }
+    public decimal earningsTimestampStart { get; set; }
+    public decimal earningsTimestampEnd { get; set; }
+    public decimal trailingAnnualDividendRate { get; set; }
+    public decimal trailingPE { get; set; }
+    public string shortName { get; set; }
+    public string displayName { get; set; }
+    public string symbol { get; set; }
+}
+
+public class Option
+{
+    public decimal expirationDate { get; set; }
+    public bool hasMiniOptions { get; set; }
+    public object[] calls { get; set; }
+    public object[] puts { get; set; }
+}
